9f186150d18e04ee2b29cfa811789a8ebd514f66
Guido Ambiorix <guidoa_amparo@jce.do>
2025-01-03 10:08:12 -0400

    --Primer hola mundo completado

--- 02-intro-vue/app.js
+++ 02-intro-vue/app.js
@@ -2,9 +2,20 @@ const { createApp, ref } = Vue;
 
 const app = createApp({
   template: `
-  <h1>Hola Mundo</h1>
+  <h1>{{message}}</h1>
   <p>Desde App.Js</p>
     `,
+
+  setup() {
+    const message = ref("I'm batman");
+
+    setTimeout(() => {
+      message.value = "Soy goku";
+    }, 5000);
+    return {
+      message,
+    };
+  },
 });
 
 app.mount("#myApp");
