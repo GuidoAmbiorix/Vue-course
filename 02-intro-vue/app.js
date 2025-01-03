bc7adbd7bf72cb23a6b02d6fb85815fba1225324
Guido Ambiorix <guidoa_amparo@jce.do>
2025-01-03 10:18:06 -0400

    html separado

--- 02-intro-vue/app.js
+++ 02-intro-vue/app.js
@@ -1,10 +1,10 @@
 const { createApp, ref } = Vue;
 
 const app = createApp({
-  template: `
-  <h1>{{message}}</h1>
-  <p>Desde App.Js</p>
-    `,
+  //   template: `
+  //   <h1>{{message}}</h1>
+  //   <p>Desde App.Js</p>
+  //     `,
 
   setup() {
     const message = ref("I'm batman");
