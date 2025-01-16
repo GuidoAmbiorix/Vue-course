05936ac33e95764eefd8d169f82543fa8054b202
guidoazaun <gamparo@zaun.do>
2025-01-15 20:40:56 -0400

    --Se integro el api

---
+++ 07-admin-shop/src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- component -->
+  <!-- Header Navbar -->
+  <TopMenu />
+
+  <!-- Home -->
+  <RouterView />
+
+  <!-- Footer -->
+  <CustomFooter />
+</template>
+
+<script setup lang="ts">
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
